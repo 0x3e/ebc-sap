7d8bd655a5cc3528cd02a09f701be9f8f360b6b0
0x 3E <0x3e.com@gmail.com>
2024-11-29 07:40:03 +1100

    improve tester

--- test/ttester.tt.js
+++ test/ttester.tt.js
@@ -1,5 +1,4 @@
-const { Ttester } = await import('./ttester.js')
-const t = Ttester(true)
+const t = await import('./ttester.js')
 
 t.isTruthy('true is truthy', () => true)
 t.isFalsy('0 is falsy', () => 0)
@@ -10,3 +9,5 @@ t.isFalse('"true" is not true', () => 'true' === true)
 t.isNull('null is null', () => null)
 t.isUndefined('undefined is undefined', () => undefined)
 t.matchObject('ob testing one 2', () => ({ one: 2 }), { one: 2 })
+t.matches('false is boolean' , () => typeof(false), 'boolean')
+t.matches('true is boolean' , () => typeof(true), 'boolean')
