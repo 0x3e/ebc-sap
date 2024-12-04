115d66fa4b16227baeb67db0f182270342edbd28
0x 3E <0x3e.com@gmail.com>
2024-12-05 00:13:49 +1100

    .

--- test/run_tests.js
+++ test/run_tests.js
@@ -3,10 +3,10 @@ import fs from "node:fs"
 const files = fs.readdirSync("test/")
 const jsFiles = files.filter(f => f.endsWith(".tt.js"))
 for (const file of jsFiles) {
-  let result = spawnSync("node", [`test/${file}`], {shell: true, stdio: "inherit"})
-  if(result.status === 0)
-    console.log(`ok - ${file}`)
-  else
-    console.log(`not ok - ${file}`)
-
+  const result = spawnSync("node", [`test/${file}`], {
+    shell: true,
+    stdio: "inherit",
+  })
+  if (result.status === 0) console.log(`ok - ${file}`)
+  else console.log(`not ok - ${file}`)
 }
