a2b88e095f93b229fe786eed7d95a451a4cac002
0x 3E <0x3e.com@gmail.com>
2024-12-07 12:21:25 +1100

    2

--- src/d_latch.display.js
+++ src/d_latch.display.js
@@ -7,21 +7,20 @@
 *                              -----                                       *
 \*                                                                        */
 
-// I will update q and expect to be sent d and en I will keep no state except where I display
 export class DLatchDisplay {
   #d = "?"
   #en = "?"
   #q = "?"
   #not_q = "?"
 
-  txt = () => `/*                     *\\
-*       DLatch          *
-*        _____          *
-*  D -${this.#d}--|   |--${this.#q}- Q    *
-*        |   |          *
-* EN -${this.#en}--|   |--${this.#not_q}- NOTQ *
-*        -----          *
-\\*                     */`
+  txt = () => `\
+       DLatch          
+        _____          
+  D -${this.#d}--|   |--${this.#q}- Q    
+        |   |          
+ EN -${this.#en}--|   |--${this.#not_q}- NOTQ 
+        -----          `
+
   set D(d) {
     this.#d = d ? 1 : 0
   }
@@ -33,5 +32,12 @@ export class DLatchDisplay {
   set Q(q) {
     this.#q = q ? 1 : 0
     this.#not_q = q ? 0 : 1
+  }
+
+  set fromJSON(json){
+    this.D = json.D
+    this.EN = json.EN
+    this.Q = json.Q
+    this.#not_q = json.NOTQ
   }
 }
--- test/d_latch.display.tt.js
+++ test/d_latch.display.tt.js
@@ -5,12 +5,48 @@ import {describe, it} from "./ttester.js"
 //NOTbiome-ignore format: text alignment
 describe("DLatchDisplay", () => {
   const dLd = new DLatchDisplay()
-  console.log(dLd.txt())
+  it.matches("is undefined DLatch", () => dLd.txt(), `\
+       DLatch          
+        _____          
+  D -?--|   |--?- Q    
+        |   |          
+ EN -?--|   |--?- NOTQ 
+        -----          \
+`)
   dLd.D = true
+  it.matches("is D=1 DLatch", () => dLd.txt(), `\
+       DLatch          
+        _____          
+  D -1--|   |--?- Q    
+        |   |          
+ EN -?--|   |--?- NOTQ 
+        -----          \
+`)
   dLd.EN = true
+  it.matches("is D=1 EN=1 DLatch", () => dLd.txt(), `\
+       DLatch          
+        _____          
+  D -1--|   |--?- Q    
+        |   |          
+ EN -1--|   |--?- NOTQ 
+        -----          \
+`)
   dLd.Q = true
-  console.log(dLd.txt())
+  it.matches("is D=1 EN=1 Q=1 DLatch", () => dLd.txt(), `\
+       DLatch          
+        _____          
+  D -1--|   |--1- Q    
+        |   |          
+ EN -1--|   |--0- NOTQ 
+        -----          \
+`)
   dLd.Q = false
-  console.log(dLd.txt())
-  it.isFalse("is a DLatch", () => dLd.isBrowser)
+  it.matches("is D=1 EN=1 Q=0 DLatch", () => dLd.txt(), `\
+       DLatch          
+        _____          
+  D -1--|   |--0- Q    
+        |   |          
+ EN -1--|   |--1- NOTQ 
+        -----          \
+`)
 })
