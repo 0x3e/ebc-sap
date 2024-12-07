365eafb2ea16d0e9a15f5ac739f1fea659c372ab
0x 3E <0x3e.com@gmail.com>
2024-12-07 17:28:12 +1100

    use webpack for uvu

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,12 +1,11 @@
-import path from "path";
+import path from "node:path"
 const config = {
   entry: "./test/test-modules.js",
   mode: "development",
   output: {
     path: path.resolve("./dist"),
-    filename: 'test-bundle.js'
-  }
+    filename: "test-bundle.js",
+  },
 }
 
-export default config;
-
+export default config
