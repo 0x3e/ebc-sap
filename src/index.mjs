a9acab95e83a80fe8d390be21c4f11c8a5e55ec7
0x 3E <0x3e.com@gmail.com>
2024-12-07 21:27:54 +1100

    update d_latch display

---
+++ src/index.mjs
@@ -0,0 +1,8 @@
+import * as h from "./helpers.js"
+import * as Gates from './gates.js'
+import {SRLatch} from "./sr_latch.js"
+import {DLatch} from "./d_latch.js"
+import {DFlipFlop} from "./d_flipflop.js"
+import {ANDEdgeDetector} from "./and_edge_detector.js"
+import {DLatchDisplay} from "./d_latch_display.js"
+export {h, Gates, SRLatch, DLatch, DFlipFlop, DLatchDisplay} 
