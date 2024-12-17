6f584af4912632fb15fac30c2f883d37910aef94
0x 3E <0x3e.com@gmail.com>
2024-12-17 17:29:57 +1100

    bus

--- test/adders.test.js
+++ test/adders.test.js
@@ -1,4 +1,4 @@
-import {describe, eq, dEq, it, ok} from "../lib/test.mjs"
+import {dEq, describe, eq, it, ok} from "../lib/test.mjs"
 import {EightBitAdder, FullAdder, HalfAdder} from "../src/adders.mjs"
 import * as h from "../src/helpers.mjs"
 
@@ -9,10 +9,13 @@ describe("Adders", () => {
     half_adder.A = false
     half_adder.B = false
     eq(half_adder.type, "HalfAdder")
-    dEq(
-      half_adder.toJSON(),
-      {"type":"HalfAdder","A":false,"B":false,"SUM":false,"CARRY":false},
-    )
+    dEq(half_adder.toJSON(), {
+      type: "HalfAdder",
+      A: false,
+      B: false,
+      SUM: false,
+      CARRY: false,
+    })
     half_adder.A = true
     eq(half_adder.SUM, true)
     eq(half_adder.CARRY, false)
@@ -33,10 +36,14 @@ describe("Adders", () => {
     full_adder.B = false
     full_adder.C = false
     eq(full_adder.type, "FullAdder")
-    dEq(
-      full_adder.toJSON(),
-      {"type":"FullAdder","A":false,"B":false,"C":false,"SUM":false,"CARRY":false},
-    )
+    dEq(full_adder.toJSON(), {
+      type: "FullAdder",
+      A: false,
+      B: false,
+      C: false,
+      SUM: false,
+      CARRY: false,
+    })
     full_adder.A = true
     eq(full_adder.SUM, true)
     eq(full_adder.CARRY, false)
@@ -62,37 +69,67 @@ describe("Adders", () => {
     eight_bit_adder.B = h.bytes.x00
     eight_bit_adder.C = false
     eq(eight_bit_adder.type, "EightBitAdder")
-    dEq(
-      eight_bit_adder.toJSON(),
-      {"type":"EightBitAdder","A": h.bytes.x00,"B": h.bytes.x00,"C":false,"SUM": h.bytes.x00,"CARRY":false},
-    )
+    dEq(eight_bit_adder.toJSON(), {
+      type: "EightBitAdder",
+      A: h.bytes.x00,
+      B: h.bytes.x00,
+      C: false,
+      SUM: h.bytes.x00,
+      CARRY: false,
+    })
     eight_bit_adder.A = h.bytes.x01
     eight_bit_adder.B = h.bytes.x00
     eight_bit_adder.C = false
-    dEq(
-      eight_bit_adder.toJSON(),
-      {"type":"EightBitAdder","A":[false,false,false,false,false,false,false,true],"B":[false,false,false,false,false,false,false,false],"C":false,"SUM":[false,false,false,false,false,false,false,true],"CARRY":false},
-    )
+    dEq(eight_bit_adder.toJSON(), {
+      type: "EightBitAdder",
+      A: [false, false, false, false, false, false, false, true],
+      B: [false, false, false, false, false, false, false, false],
+      C: false,
+      SUM: [false, false, false, false, false, false, false, true],
+      CARRY: false,
+    })
     eight_bit_adder.A = h.bytes.x01
     eight_bit_adder.B = h.bytes.x01
-    dEq(
-      eight_bit_adder.toJSON(),
-      {"type":"EightBitAdder","A":[false,false,false,false,false,false,false,true],"B":[false,false,false,false,false,false,false,true],"C":false,"SUM":[false,false,false,false,false,false,true,false],"CARRY":false},
-    )
-    dEq(
-      eight_bit_adder.SUM,
-      [false, false, false, false, false, false, true, false],
-    )
+    dEq(eight_bit_adder.toJSON(), {
+      type: "EightBitAdder",
+      A: [false, false, false, false, false, false, false, true],
+      B: [false, false, false, false, false, false, false, true],
+      C: false,
+      SUM: [false, false, false, false, false, false, true, false],
+      CARRY: false,
+    })
+    dEq(eight_bit_adder.SUM, [
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      true,
+      false,
+    ])
     eight_bit_adder.A = h.bytes.x0F
     eight_bit_adder.B = h.bytes.x0F
-    dEq(
-      eight_bit_adder.SUM,
-      [false, false, false, true, true, true, true, false],
-    )
+    dEq(eight_bit_adder.SUM, [
+      false,
+      false,
+      false,
+      true,
+      true,
+      true,
+      true,
+      false,
+    ])
     eight_bit_adder.B = h.bytes.x01
-    dEq(
-      eight_bit_adder.SUM,
-      [false, false, false, true, false, false, false, false],
-    )
+    dEq(eight_bit_adder.SUM, [
+      false,
+      false,
+      false,
+      true,
+      false,
+      false,
+      false,
+      false,
+    ])
   })
 })
