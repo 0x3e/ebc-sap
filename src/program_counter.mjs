4863441827ab51aaf5e3555e130c14ea6b6853c8
0x 3E <0x3e.com@gmail.com>
2025-03-10 17:45:24 +1100

    program counter

--- src/program_counter.mjs
+++ src/program_counter.mjs
@@ -1,11 +1,40 @@
 import * as Gates from "./gates.mjs"
 import * as h from "./helpers.mjs"
+import {JKFlipFlop} from "./jk_flipflop.mjs"
 import {PubSub} from "./pub_sub.mjs"
 
 export class ProgramCounter {
-  #JUMP = undefined
   #OUT = undefined
-  #LOAD = undefined
+  #JUMP = undefined
+  #ENABLE = undefined
   #Q = undefined
   #tristate = undefined
+
+  static type = "ProgramCounter"
+
+  constructor() {
+    this.#tristate = h.ArrayOf(4, () => new Gates.TriState())
+  }
+
+  get output() {
+    return this.#Q
+  }
+
+  get type() {
+    return this.constructor.type
+  }
+
+  toJSON() {
+    return {
+      type: this.type,
+      OUT: this.#astable_pulse,
+      JUMP: this.#interval,
+      ENABLE: this.#select,
+      Q: this.#Q,
+    }
+  }
+
+  toString() {
+    return JSON.stringify(this.toJSON())
+  }
 }
